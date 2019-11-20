12d9416dae62b60f6c35fb4560de6e18bf701761
Remigiusz Rajca <remigiusz.rajca@gmail.com>
2019-11-20 23:46:48 +0100

    add new class Statistics

---
+++ Statistics.js
@@ -0,0 +1,21 @@
+class Statistics {
+    constructor() {
+        this.gamesPlayed = [];
+    };
+
+    addGameResult(win, bid) {
+        const result = {
+            win,
+            bid
+        };
+
+        this.gamesPlayed.push(result);
+    };
+
+    getGameResults() {
+        const games = this.gamesPlayed.length;
+        const wins = this.gamesPlayed.filter(game => game.win).length;
+        const loses = this.gamesPlayed.filter(game => !game.win).length;
+        return [games, wins, loses];
+    };
+};
